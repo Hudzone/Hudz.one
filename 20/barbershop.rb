5e00b747dda7ca869e4194d6d1bf31c825f6bfa3
Hudzone <fyodor.blyudov@gmail.com>
2023-10-01 20:54:59 +0300

    added .admin zone

--- 20/barbershop.rb
+++ 20/barbershop.rb
@@ -13,8 +13,26 @@ post '/' do
   @message = "Dear, #{@user_name}, thank you for your apply! Waiting for you at #{@date_time}"
 
   f = File.open 'users.txt', 'a'
-  f.write "#{@user_name} (phone: #{@phone}) booked at #{@date_time}."
+  f.write "#{@user_name} (phone: #{@phone}) booked at #{@date_time}\n"
   f.close
 
   erb :message
+end
+
+get '/adm_result' do
+  erb :admin
+end
+
+post '/adm_result' do
+  @user_name = params[:login]
+  @password = params[:password]
+
+  if @user_name == 'admin' && @password == 'admin'
+    @users = File.open("users.txt","r")
+    erb :adm_result
+  else
+    @oth = '<p>Access denied! Wrong login or password.</p>'
+    erb :admin
+  end
+
 end
