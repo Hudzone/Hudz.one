54f78bcc62a1d527d264f6c16e1a42b5cab6c603
Hudzone <fyodor.blyudov@gmail.com>
2023-11-08 21:12:50 +0300

    added Lesson 21

---
+++ 21/barbershop.rb
@@ -0,0 +1,39 @@
+require 'sinatra'
+require 'bootstrap'
+
+get '/' do
+  erb :welcome
+end
+
+post '/' do
+  @user_name = params[:user_name]
+  @phone = params[:phone]
+  @date_time = params[:date_time]
+
+  @title = 'CONGRATS!'
+  @message = "Dear, #{@user_name}, thank you for your apply! Waiting for you at #{@date_time}"
+
+  f = File.open './public/users.txt', 'a'
+  f.write "#{@user_name} (phone: #{@phone}) booked at #{@date_time}\n"
+  f.close
+
+  erb :message
+end
+
+get '/adm_result' do
+  erb :admin
+end
+
+post '/adm_result' do
+  @user_name = params[:login]
+  @password = params[:password]
+
+  if @user_name == 'admin' && @password == 'admin'
+    @users = File.open("./public/users.txt","r")
+    erb :adm_result
+  else
+    @oth = '<p>Access denied! Wrong login or password.</p>'
+    erb :admin
+  end
+
+end
