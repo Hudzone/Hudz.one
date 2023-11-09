598d42e72a60775ed1fd148435fc7c9b301e9d1c
Hudzone <fyodor.blyudov@gmail.com>
2023-11-09 21:47:23 +0300

    Lesson 22

--- 22/app.rb
+++ 22/app.rb
@@ -5,3 +5,19 @@ require 'sinatra/reloader'
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
+
+get '/termin' do 
+  erb :termin
+end
+
+get '/visit' do
+  erb :visit
+end
+
+get '/something' do
+  erb :about
+end
+
+get '/contacts' do 
+  erb :contacts
+end
