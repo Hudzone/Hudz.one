41931165cd7498b087e23c4ab82405de7b7af76b
Hudzone <fyodor.blyudov@gmail.com>
2023-11-09 23:17:50 +0300

    Lesson 22: tasks finished

--- 22/app.rb
+++ 22/app.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb "Добро пожаловать на сайт нашего барбершопа! Мы открылись и рады новым клиентам"			
 end
 
 get '/termin' do 
@@ -14,6 +14,19 @@ get '/visit' do
   erb :visit
 end
 
+post '/visit' do 
+  @usrname = params[:username]
+  @datetime = params[:date_time]
+  @phonenumber = params[:phone_number]
+  @brb = params[:barber]
+
+  f = File.open './public/users.txt', 'a'
+  f.write "#{@usrname} (телефон #{@phonenumber}) записался(сь) на #{@datetime} к #{@brb}\n"
+  f.close
+
+  erb :visit
+end
+
 get '/something' do
   erb :about
 end
@@ -21,3 +34,24 @@ end
 get '/contacts' do 
   erb :contacts
 end
+
+post '/contacts' do
+  @mail = params[:e_mail]
+  @feedback = params[:story]
+
+  kj = File.open './public/feedback.txt', 'a'
+  kj.write "=============================================================================\n"
+  kj.write "Электронная почта: #{@mail}\n"
+  kj.write "Отзыв пользователя: \n"
+  kj.write "#{@feedback}\n"
+  kj.write "=============================================================================\n"
+  kj.close
+
+  yt = File.open './public/contacts.txt', 'a'
+  yt.write "#{@mail}\n"
+  yt.close
+
+  erb :contacts
+end
+
+
